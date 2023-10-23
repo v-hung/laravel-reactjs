8c603fb007096d19edb96e7b09055df80951385a
Viet Hung <viet.hung.2898@gmail.com>
2023-10-23 22:30:12 +0700

    add

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Document</title>
+  <title>💻 Học tài thi phận - KennaTech</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block" />
   
   @viteReactRefresh
