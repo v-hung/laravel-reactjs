e4b4c149265d9f393b87e8f7372581afde8f812e
Viet Hung <viet.hung.2898@gmail.com>
2023-10-24 14:26:38 +0700

    add

---
+++ public/build/assets/modal-a5743ae9.js
@@ -0,0 +1,1 @@
+import{R as s}from"./main-ea3bcec7.js";const u=s((t,p)=>({isOpen:null,dispatch:e=>t(o=>typeof e=="function"?e(o):e)}));export{u};
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>💻 Học tài thi phận - KennaTech</title>
+  <meta name="description" content="Trang web dạy học và thi trực tuyến KennaTech">
+
+  <meta property="og:image" content="{{ asset('asset/img/thi-cu.png') }}" />
+  <meta property="og:url" content="{{ config('app.url') }}" />
+  <meta property="og:type" content="Website" />
+  <meta property="og:title" content="💻 Học tài thi phận - KennaTech" />
+  <meta property="og:description" content="Trang web dạy học và thi trực tuyến KennaTech" />
+
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block" />
   
   @viteReactRefresh
